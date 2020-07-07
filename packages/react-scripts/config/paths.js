e8569aa45fcfbf4635ecd258e2154c5adf9c9b66
Brenton Holswich <brenton.holswich@gmail.com>
2020-07-06 20:47:49 -0700

    updated to include cast html and publish name

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -85,6 +85,7 @@ module.exports = {
   appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
+  appCastHtml: resolveApp('public/cast.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
